effe640f9504a9d5700f7c9fde20c674729f1806
Sayali Khot <sayalikhot1801@gmail.com>
2023-02-04 11:42:28 +0530

    JOSN file created full home page

---
+++ src/Data.js
@@ -0,0 +1,27 @@
+const Data=[
+    {
+        id:1, 
+        img:"https://cdn.pixabay.com/photo/2023/01/26/18/09/zebra-7746719_1280.jpg"
+    },
+    {
+        id:2,
+        img:"https://cdn.pixabay.com/photo/2023/01/26/18/09/zebra-7746719_1280.jpg"
+
+        },
+    {
+        id:3,
+        img:"https://cdn.pixabay.com/photo/2023/01/26/18/09/zebra-7746719_1280.jpg"
+
+    },
+    {
+        id:4,
+        img:"https://cdn.pixabay.com/photo/2023/01/26/18/09/zebra-7746719_1280.jpg"
+
+    },
+    {
+        id:5,
+        img:"https://cdn.pixabay.com/photo/2023/01/26/18/09/zebra-7746719_1280.jpg"
+
+    },
+];
+export default Data;
---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+function Navbar(){
+    return(
+        <div className="navbar-main">
+        <div className="navbar-left">
+
+        </div>
+        <div className="navbar-right">
+            <ul>
+                <a href=""> <li>Home</li></a>
+
+                    <a href=""><li>About Us</li></a>
+                    <a href=""><li>Products</li></a>
+                    <a href=""><li>Testimonials</li></a>
+                    <a href=""><li>Contact</li></a>
+                    <a href=""><li><button>Get Quote</button></li></a>
+
+
+            </ul>
+        </div>
+        </div>
+    )
+
+}
+export default Navbar;
