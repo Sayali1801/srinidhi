736b9288c70e44fe6fd6bcf1e17372d1d87b0bfc
Sayali Khot <sayalikhot1801@gmail.com>
2023-02-10 13:03:21 +0530

    scrolling part of out client

--- src/components/OurClients.js
+++ src/components/OurClients.js
@@ -1,22 +1,41 @@
-import Data from '../Data.json';
-import '../Styles/OurClients.css';
-function OurClients(){
-    return(
-        <div className='ourClient-main' id='our-client'>
-        <div className='ourClient-up'>
-            <h1>{Data.home_page.our_clients.our_clients_title}</h1>
-            <p>{Data.home_page.our_clients.our_clients_desc}</p>
+// import Slider from 'react-slick';
+import Data from "../Data.json";
+import "../Styles/OurClients.css";
+function OurClients() {
+  // const display = {
+  //     dots:false,
+  //     infinite: true,
+  //     speed: 500,
+  //     autoplaySpeed: 2000,
+  //     autoplay: true,
+  //     slidesToShow: 5,
+  //     slidesToScroll: 5,
+  //     arrows:false
 
-        </div>
-        <div className='ourClient-down'>
-                   {Data.home_page.our_clients.our_clients_logo.map((item)=>{
-                return(
-                    <img  src={require('../Images/' + item)} alt="client logo"/>
-                )
-            })}
- 
-        </div>
-        </div>
-    )
+  // };
+  return (
+    <div className="ourClient-main" id="our-client">
+      <div className="ourClient-up">
+        <h1>{Data.home_page.our_clients.our_clients_title}</h1>
+        <p>{Data.home_page.our_clients.our_clients_desc}</p>
+      </div>
+
+      <div className="ourClient-down">
+        {Data.home_page.our_clients.our_clients.map((item) => {
+          return (
+            <div className="our-clients">
+              <div className="our-clients-scroll">
+                <img
+                  src={require("../Images/" + item.logo)}
+                  alt="client logo"
+                />
+                <p>{item.name}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 export default OurClients;
